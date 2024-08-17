b800822c61bbb205dec1bd72c041b5b3708e4f54
JW5500 <89821850+JW5500@users.noreply.github.com>
2024-08-17 01:42:20 -0400

    in class code done

--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { configureStore } from '@reduxjs/toolkit'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import { employeesReducer } from './employeesSlice'
+import { tasksReducer } from './tasksSlice';
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const store = configureStore({
+  reducer: {
+    // Define a top-level state field
+    employees: employeesReducer,
+    tasks: tasksReducer,
+  }
+})
+
+export default store;
